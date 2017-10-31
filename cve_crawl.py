9d9cd4ff255499671415f7c65b0017fa5d0d10d5
pinjk123 <pinjk12@naver.com>
2017-10-31 15:18:10 +0900

    add exploit-db crwal

--- cve_crawl.py
+++ cve_crawl.py
@@ -3,10 +3,33 @@
 '''
 
 from bs4 import BeautifulSoup as BS
-import urllib.request as ulib
+from urllib.request import urlopen, Request, urlretrieve
 import os
 import time
 import csv
+import random
+import re
+
+user_agent_list = [
+             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+             "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+             "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+             "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+             "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+             "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+             "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+             "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+             "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+             "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+             "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+             "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+             "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+            ]
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 cve_lists = os.listdir(base_dir + "\\cve_list")
@@ -23,14 +46,13 @@ def csv_writer(data, cve_number):
 #--------------------------------------------------------------------------------------------------------------
 def cve_crawler(url):
 
-	data = ulib.urlopen(url).read()
+	data = urlopen(Request(url)).read()
 	soup = BS(data, "html.parser")
 	result_data = []
 
 	letters = soup.find_all("div", id="GeneratedTable")
 
-	cnt = 1
-	end = True
+	cnt = 0
 	for result in letters:
 
 		cve_number = result.contents[1].find_all("td")[0].get_text().replace("\n", "")
@@ -40,29 +62,59 @@ def cve_crawler(url):
 		result_data.append(url)
 
 		for ref in result.contents[1].find_all("td")[4].get_text().strip().split("\n"):
+			
 			if "exploit-db" in ref:
 				edb_url = soup.find("a", text=ref)['href']
 				result_data.append(edb_url)
-			elif end:
-				end = False
-				result_data.append("No Exploit-DB")
+				edb_data = edb_crwal(cve_number, edb_url)
+
+				for data in edb_data:
+					result_data.append(data)
+
+				cnt = cnt + 1
+
+		if cnt == 0:
+			result_data.append("No Exploit-DB")
 
 		print(result_data)
 		return result_data
 
+#------------------------------------------------------------------------------
+def edb_crwal(cve_number, url):
 
+	global user_agent_list
+
+	user_agent = random.choice(user_agent_list)
+	headers = {'User-Agent' : user_agent}
+	data = urlopen(Request(url, None, headers= headers)).read()
+	soup = BS(data, "html.parser", from_encoding="ISO-8859-1")
+
+	platform = soup.find("a", href=re.compile("www.exploit-db.com/platform")).text
+	ttype = soup.find("strong", text="Type").next_sibling.next_sibling.text
+	exploit = soup.find("a", href=re.compile("www.exploit-db.com/download"))['href']
+	
+	if exploit != None:
+		urlretrieve(exploit, base_dir + "\\cve_list\\" + cve_number + "\\" + cve_number + "_exploit.txt")
+
+	return [platform, ttype, exploit]
 
 #------------------------------------------------------------------------------
 if __name__ == "__main__":
 
 	for cnum in cve_lists:
 		url = "https://cve.mitre.org/cgi-bin/cvename.cgi?name=" + cnum
-		cve_data = [["CVE NUMBER", "CVE Description", "CVE URL", "Exploit-DB URL"], cve_crawler(url)]
+		cve_data = [
+			[
+				"CVE NUMBER", "CVE Description", "CVE URL", "Exploit-DB URL",
+				"Exploit Platform", "Exploit Type", "Exploit Code"
+			], 
+			cve_crawler(url)
+		]
+		# json으로 map - mongodb
 		csv_writer(cve_data, cnum)
 
 		time.sleep(1)
 
 
-
 #TODO : SAVE INFO AS EXCEL / MONGODB
-# ebd_url 추출했음 / cve number / cve description / cve url 추출했음 / exploit code랑 프로그램 추출못했음
+# 프로그램 추출못했음
